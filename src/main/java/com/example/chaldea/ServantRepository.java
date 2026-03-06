9c12f358606ffcbe1a573ecd3df5b5fc9e9a1e7d
seihaisensou <awakeningtxt@gmail.com>
2026-03-06 18:18:34 -0500

    Servant Repository created containing basic queries

---
+++ src/main/java/com/example/chaldea/ServantRepository.java
@@ -0,0 +1,22 @@
+package com.example.chaldea;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ServantRepository extends JpaRepository<Servant, Long> {
+
+  List<Servant> findByType(String type);
+
+  @Query(value = "SELECT s.* FROM students s WHERE s.origin like %?1%", nativeQuery = true)
+  List<Servant> findbyOrigin(String origin);
+
+  @Query(value = "SELECT s.* FROM students s WHERE s.name like %?1%", nativeQuery = true)
+  List<Servant> findByName(String name);
+
+  Servant findBySpecies(String species);
+
+}
