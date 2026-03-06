7d14467507440bb5bb9b381a7480ce8a225d88cd
seihaisensou <awakeningtxt@gmail.com>
2026-03-06 18:29:20 -0500

    ServantService created and implements methods created by ServantRepository

---
+++ src/main/java/com/example/chaldea/ServantService.java
@@ -0,0 +1,60 @@
+package com.example.chaldea;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServantService {
+
+  private final ServantRepository servantRepository;
+
+  public ServantService(ServantRepository servantRepository) {
+    this.servantRepository = servantRepository;
+  }
+
+  public List<Servant> getAllServants() {
+    return servantRepository.findAll();
+  }
+
+  public Servant createServant(Servant servant) {
+    return servantRepository.save(servant);
+  }
+
+  public Servant getServantById(Long id) {
+    return servantRepository.findById(id).orElse(null);
+  }
+
+  public Servant updateServant(Long id, Servant updatedServant) {
+    return servantRepository.findById(id)
+        .map(servant -> {
+          servant.setName(updatedServant.getName());
+          servant.setType(updatedServant.getType());
+          servant.setSpecies(updatedServant.getSpecies());
+          servant.setOrigin(updatedServant.getOrigin());
+          return servantRepository.save(servant);
+        })
+        .orElse(null);
+  }
+
+  public void deleteServant(Long id) {
+    servantRepository.deleteById(id);
+  }
+
+  public List<Servant> getServantsByType(String type) {
+    return servantRepository.findByType(type);
+  }
+
+  public List<Servant> getServantsByOrigin(String origin) {
+    return servantRepository.findbyOrigin(origin);
+  }
+
+  public List<Servant> searchServantsByName(String name) {
+    return servantRepository.findByName(name);
+  }
+
+  public Servant getServantBySpecies(String species) {
+    return servantRepository.findBySpecies(species);
+  }
+
+}
