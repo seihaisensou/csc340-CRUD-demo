ae5967553ba66cae6331a246f90a1fa546029a36
seihaisensou <awakeningtxt@gmail.com>
2026-03-06 17:49:52 -0500

    inserted SpringBoot project contents into repository

---
+++ src/main/java/com/example/chaldea/ChaldeaApplication.java
@@ -0,0 +1,13 @@
+package com.example.chaldea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaldeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaldeaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chaldea/ChaldeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chaldea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaldeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
